2b1cf21e229a4bf828e615d56364372a7817dc9e
Ty Hawkes <hawkety77@gmail.com>
2025-11-13 12:36:21 -0700

    ty code

--- ty_code.r
+++ ty_code.r
@@ -16,18 +16,10 @@ set.seed(77)
 tsne_result <- Rtsne(df, perplexity=30)
 tsne_df_base <- data.frame(X = tsne_result$Y[,1], Y = tsne_result$Y[,2])
 
-# t-SNE on scaled data with DBSCAN clusters
-tsne_df_dbscan <- data.frame(X=tsne_result$Y[,1], Y=tsne_result$Y[,2], Cluster=dbscan_cluster)
-ggplot(tsne_df_dbscan, aes(x=X, y=Y, color=Cluster)) +
-  geom_point(alpha=0.7, size=2) +
-  theme_minimal() +
-  labs(title="t-SNE Visualization (DBSCAN Clusters)", 
-       x="t-SNE Dimension 1", y="t-SNE Dimension 2")
-
 # k-means clustering with varying k
 par(mfrow=c(2,3))
 
-for (k in 3:7) {
+for (k in 2:7) {
   set.seed(77)
   kmeans_result <- kmeans(df, centers=k, nstart=20)
   kmeans_cluster <- as.factor(kmeans_result$cluster)
